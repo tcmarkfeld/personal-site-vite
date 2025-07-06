76c7b8c851dd0e8466293c9b12678e3d6e8b942d
tcmarkfeld <timmarkfeld@gmail.com>
2025-07-05 19:13:42 -0500

    remove react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen.ts';
 import { Spinner } from './components/ui/spinner.tsx';
@@ -18,8 +17,6 @@ const router = createRouter({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <RouterProvider router={router} />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
